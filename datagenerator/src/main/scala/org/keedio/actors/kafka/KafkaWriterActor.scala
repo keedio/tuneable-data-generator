f7140c565419fc32506c56ac88dd37dc0ae61476
Luca Rosellini <luca.rosellini@gmail.com>
2015-02-18 12:38:44 +0100

    Implemented KafkaWriterActor logic

--- datagenerator/src/main/scala/org/keedio/actors/kafka/KafkaWriterActor.scala
+++ datagenerator/src/main/scala/org/keedio/actors/kafka/KafkaWriterActor.scala
@@ -1,8 +1,14 @@
 package org.keedio.actors.kafka
 
+import java.util.{Properties, UUID}
+
+import akka.actor.Actor
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import kafka.producer.{Producer, ProducerConfig, KeyedMessage}
+import org.bson.BSONObject
 import org.keedio.actors.KafkaProducerActor
 import org.keedio.common.message.Stop
+import org.keedio.config.KafkaConfig
 import org.keedio.datagenerator.domain.{DeleteTransaction, SaveAccount, SaveTransaction}
 import org.keedio.domain.{Account, Transaction}
 import org.springframework.beans.factory.config.BeanDefinition
@@ -14,12 +20,34 @@ import org.springframework.stereotype.Component
  */
 @Component("kafkaWriterActor")
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
-class KafkaWriterActor extends KafkaProducerActor with LazyLogging {
+class KafkaWriterActor extends Actor with LazyLogging with KafkaConfig{
+
+  val props = new Properties()
+
+  props.put("metadata.broker.list", kafkaBrokers);
+  props.put("serializer.class", "kafka.serializer.StringEncoder");
+  props.put("key.serializer.class", "kafka.serializer.StringEncoder");
+  props.put("request.required.acks", requiredAcks);
+
+  val producerConfig = new ProducerConfig(props);
+  val producer = new Producer[String, String](producerConfig)
+
   override def receive: PartialFunction[Any, Unit] = {
-    case SaveTransaction(tx:Transaction) => //txRepo.save(tx)
-    case SaveAccount(account:Account) => //accRepo.save(account)
-    case DeleteTransaction(tx:Transaction) => //txRepo.delete(tx.getId)
+    case SaveTransaction(tx:Transaction) => {
+      val msg = new KeyedMessage[String,String](topic, UUID.randomUUID().toString,tx.toString)
+      producer.send(msg)
+    }
+    case SaveAccount(account:Account) => {
+      val msg = new KeyedMessage[String,String](topic, UUID.randomUUID().toString,account.toString)
+      producer.send(msg)
+    }
+    case DeleteTransaction(tx:Transaction) => {
+      val msg = new KeyedMessage[String,String](topic, UUID.randomUUID().toString,tx.toString)
+      producer.send(msg)
+    }
     case Stop() => context stop self
     case _ => logger.error("es.care.sf.business.common.message.Message not recognized")
   }
+
+  protected def topic: String = "infinito"
 }
